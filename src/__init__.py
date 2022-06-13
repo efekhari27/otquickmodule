4b88803e30f67ca8a66bba4b9a85a277663803ce
efekhari <elias.fekhari@gmail.com>
2022-06-13 17:07:26 +0200

    Create __init.py file

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+"""otquickmodule module"""
+
+from .KernelHerding import KernelHerding
+from .BayesianQuadratureWeighting import BayesianQuadratureWeighting
+
+__all__ = [
+    "KernelHerding",
+    "BayesianQuadratureWeighting",
+]
+__version__ = "0.0.1"
